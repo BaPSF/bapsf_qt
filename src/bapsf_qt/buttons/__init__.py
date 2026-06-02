45d74eee6a9ee240821bc16d852a5dcf2fe279ea
erik <eteverson@gmail.com>
2026-06-02 16:35:12 -0700

    copy StopButton and ZeroButton from bapsf_motion

--- src/bapsf_qt/buttons/__init__.py
+++ src/bapsf_qt/buttons/__init__.py
@@ -5,10 +5,12 @@ __all__ = [
     "GearButton",
     "GearValidButton",
     "IconButton",
+    "StopButton",
     "StyleButton",
     "ValidButton",
+    "ZeroButton",
 ]
 
 from bapsf_qt.buttons.core import AutoScaleButton, StyleButton, ValidButton
-from bapsf_qt.buttons.designed import DiscardButton, DoneButton
+from bapsf_qt.buttons.designed import DiscardButton, DoneButton, StopButton, ZeroButton
 from bapsf_qt.buttons.iconic import IconButton, GearButton, GearValidButton
--- src/bapsf_qt/buttons/designed.py
+++ src/bapsf_qt/buttons/designed.py
@@ -1,8 +1,9 @@
 """
 Buttons design for a specific purpose.
 """
-__all__ = ["DiscardButton", "DoneButton"]
-from bapsf_qt.buttons.core import AutoScaleButton
+__all__ = ["DiscardButton", "DoneButton", "StopButton", "ZeroButton"]
+
+from bapsf_qt.buttons.core import AutoScaleButton, StyleButton
 
 
 class DiscardButton(AutoScaleButton):
@@ -39,3 +40,35 @@ class DoneButton(AutoScaleButton):
     def _calculate_target_width(self, text: str | None = None, scale: float = 1.0):
         scale = 2 * scale
         return super()._calculate_target_width(text, scale=scale)
+
+
+class StopButton(StyleButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__("STOP", *args, **kwargs)
+
+        self.update_style_sheet(
+            styles={
+                "background-color": "rgb(232, 80, 74)",
+                "border-radius": "6px",
+                "border": "3px solid rgb(90, 90, 90)",
+                "color": "rgba(30, 30, 30, 240)",
+            },
+            action="base",
+        )
+        self.update_style_sheet(
+            styles={
+                "border": "3px solid rgb(255, 0, 0)",
+                "background-color": "rgb(255, 70, 70)",
+            },
+            action="hover",
+        )
+
+
+class ZeroButton(StyleButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.update_style_sheet(
+            styles={"background-color": "rgb(52, 161, 219)"},
+            action="pressed",
+        )
