f21a6c39fa80594ed7a885825b2c60d554ddacb1
erik <eteverson@gmail.com>
2026-06-02 13:44:33 -0700

    copy get_color_scheme() from bapsf_motion

--- src/bapsf_qt/utils/__init__.py
+++ src/bapsf_qt/utils/__init__.py
@@ -1,10 +1,20 @@
-__all__ = ["cast_color_to_rgba_string", "get_qapplication"]
+"""
+A collection of utility functionality to help build `PySide6` GUIs.
+"""
+
+from __future__ import annotations
+
+__all__ = ["cast_color_to_rgba_string", "get_color_scheme", "get_qapplication"]
 
 import ast
 
 from PySide6.QtCore import QCoreApplication
 from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QApplication
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from PySide6.QtCore import Qt
 
 
 def get_qapplication() -> QCoreApplication | None:
@@ -15,6 +25,24 @@ def get_qapplication() -> QCoreApplication | None:
     """
     app = QApplication.instance()
     return app
+
+
+def get_color_scheme() -> Qt.ColorScheme:
+    """
+    Retrieve the color scheme (light or dark mode) of the operating
+    system.  The returns the `~PySide6.QtCore.Qt.ColorScheme` `enum`.
+    """
+    app = get_qapplication()
+
+    if not hasattr(app, "styleHints"):
+        raise TypeError(
+            "Expected the main application to be an instance of 'QApplication' "
+            f"so styleHints().colorScheme() (i.e. light / dark mode) could be "
+            f"retrieved, got type {type(app)} instead."
+        )
+
+    _scheme = app.styleHints().colorScheme()
+    return _scheme
 
 
 def cast_color_to_rgba_string(color: QColor | str) -> str:
