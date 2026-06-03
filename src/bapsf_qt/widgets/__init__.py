b6af966293d8dc0ad619c2030514bf1a9c91a8a0
erik <eteverson@gmail.com>
2026-06-02 17:32:11 -0700

    copy QTAIconLabel from bapsf_motion

--- src/bapsf_qt/widgets/__init__.py
+++ src/bapsf_qt/widgets/__init__.py
@@ -2,7 +2,7 @@
 A collection of custom PySide6 widgets intended for use across the BaPSF
 GUIs.
 """
-__all__ = ["QLineEditPayload", "HLinePlain", "VLinePlain"]
+__all__ = ["QLineEditPayload", "HLinePlain", "VLinePlain", "QTAIconLabel"]
 
 from bapsf_qt.widgets.text import QLineEditPayload
-from bapsf_qt.widgets.styling import HLinePlain, VLinePlain
+from bapsf_qt.widgets.styling import HLinePlain, VLinePlain, QTAIconLabel
--- src/bapsf_qt/widgets/styling.py
+++ src/bapsf_qt/widgets/styling.py
@@ -3,14 +3,18 @@ A collection of styling (non-active) based `PySide6` widgets.
 """
 from __future__ import annotations
 
-__all__ = ["HLinePlain", "VLinePlain"]
+__all__ = ["HLinePlain", "VLinePlain", "QTAIconLabel"]
 
-from PySide6.QtGui import QColor
-from PySide6.QtWidgets import QFrame
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QColor, QIcon
+from PySide6.QtWidgets import QFrame, QLabel
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from PySide6.QtWidgets import QWidget
+
+# import of qtawesome must happen after the PySide6 imports
+import qtawesome as qta  # noqa
 
 
 class HLinePlain(QFrame):
@@ -65,3 +69,62 @@ class VLinePlain(QFrame):
         palette = self.palette()
         palette.setColor(palette.ColorRole.WindowText, QColor(r, g, b))
         self.setPalette(palette)
+
+
+class QTAIconLabel(QLabel):
+    def __init__(self, icon_name: str, parent : QWidget | None = None):
+        super().__init__(parent=parent)
+
+        self._icon_name = None
+        self._icon = None
+        self.setIcon(icon_name)
+
+        self.setFixedSize(32)
+        self.setIconSize(28)
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
+
+    @property
+    def icon_name(self) -> str:
+        return self._icon_name
+
+    @property
+    def icon(self) -> QIcon:
+        return self._icon
+
+    def _get_icon(self):
+        return qta.icon(self._icon_name)
+
+    def setIcon(self, icon_name: str):  # noqa
+        try:
+            _icon = qta.icon(icon_name)
+        except Exception:  # noqa
+            return
+
+        self._icon_name = icon_name
+        self._icon = _icon
+
+    def setIconSize(self, size: int):  # noqa
+        if not isinstance(size, int):
+            return
+        elif size < 1:
+            return
+
+        self.setPixmap(self.icon.pixmap(size, size))
+
+    def setFixedSize(self, size: QSize | int):
+        if isinstance(size, QSize):
+            pass
+        elif not isinstance(size, int):
+            return
+        elif size < 1:
+            return
+        else:
+            size = QSize(size, size)
+
+        super().setFixedSize(size)
+
+    def setFixedWidth(self, w: int):
+        self.setFixedSize(w)
+
+    def setFixedHeight(self, h: int):
+        self.setFixedSize(h)
