28a96aedf9994635ca4bf90e81277de64322f3a7
erik <eteverson@gmail.com>
2026-06-02 17:41:05 -0700

    copy IPPv4Validator from bapsf_motion

--- src/bapsf_qt/widgets/__init__.py
+++ src/bapsf_qt/widgets/__init__.py
@@ -2,7 +2,8 @@
 A collection of custom PySide6 widgets intended for use across the BaPSF
 GUIs.
 """
-__all__ = ["QLineEditPayload", "HLinePlain", "VLinePlain", "QTAIconLabel"]
+__all__ = ["QLineEditPayload", "IPv4Validator", "HLinePlain", "VLinePlain", "QTAIconLabel"]
 
 from bapsf_qt.widgets.text import QLineEditPayload
 from bapsf_qt.widgets.styling import HLinePlain, VLinePlain, QTAIconLabel
+from bapsf_qt.widgets.validators import IPv4Validator
---
+++ src/bapsf_qt/widgets/validators.py
@@ -0,0 +1,29 @@
+"""
+A collection of `PySide6` `~PySide6.QtGui.QValidator` sub-class widgets.
+"""
+__all__ = ["IPv4Validator"]
+
+import logging
+import re
+
+from PySide6.QtGui import QValidator
+
+
+class IPv4Validator(QValidator):
+    def __init__(self, logger=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._pattern = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+
+        log_name = "" if logger is None else f"{logger.name}."
+        log_name += "IPv4Validator"
+        self._logger = logging.getLogger(log_name)
+
+    def validate(self, arg__1: str, arg__2: int) -> object:
+        string = arg__1.replace("_", "")
+
+        match = self._pattern.fullmatch(string)
+        if match is None:
+            self._logger.warning(f"IP address is invalid, '{string}'.")
+            return QValidator.State.Intermediate
+
+        return QValidator.State.Acceptable
